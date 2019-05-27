c291cb9ca9350d826391e40a0b0034f1826ffd25
Nick Maliwacki <nmaliwa@microsoft.com>
2019-05-26 17:10:48 -0700

    add windows container

---
+++ data/public/precache-manifest.a9060ef801c5d2ea119791640687179e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "662557a7d1182c5435a0",
+    "url": "/static/js/main.662557a7.chunk.js"
+  },
+  {
+    "revision": "be15e4459eae8d9cda8f",
+    "url": "/static/js/2.be15e445.chunk.js"
+  },
+  {
+    "revision": "662557a7d1182c5435a0",
+    "url": "/static/css/main.eb5413a0.chunk.css"
+  },
+  {
+    "revision": "be15e4459eae8d9cda8f",
+    "url": "/static/css/2.34a231b5.chunk.css"
+  },
+  {
+    "revision": "cc48a7edd7cace177385dd16b5095848",
+    "url": "/index.html"
+  }
+];
